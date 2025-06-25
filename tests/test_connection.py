b0a2377451304617d5a32ef6ba586fed5742799e
slaily <slavov.iliyan96@gmail.com>
2025-06-25 15:43:48 +0300

    Refactor connection test cases

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,10 +1,29 @@
-import unittest
-import asyncio
 import time
-from unittest.mock import AsyncMock
+import unittest
+
+from typing import Any
+from asyncio import sleep
 
 from aiosqlitepool.connection import PoolConnection
-from aiosqlitepool.protocols import Connection
+
+
+class SQLiteConnection:
+    def __init__(self):
+        self._connection = object()
+        self.closed = False
+        self.rolled_back = False
+
+    async def execute(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+    async def rollback(self) -> None:
+        self.rolled_back = True
+        await sleep(0)
+
+    async def close(self) -> None:
+        self.closed = True
+        self._connection = None
+        await sleep(0)
 
 
 class TestPoolConnection(unittest.IsolatedAsyncioTestCase):
@@ -12,7 +31,7 @@ class TestPoolConnection(unittest.IsolatedAsyncioTestCase):
         """
         Test that a PoolConnection is created with the correct initial attributes.
         """
-        mock_conn = AsyncMock(spec=Connection)
+        mock_conn = SQLiteConnection()
         conn_id = "conn_1"
 
         pool_conn = PoolConnection(connection=mock_conn, connection_id=conn_id)
@@ -26,11 +45,11 @@ class TestPoolConnection(unittest.IsolatedAsyncioTestCase):
         """
         Test that the 'age' property correctly calculates the connection's age.
         """
-        mock_conn = AsyncMock(spec=Connection)
+        mock_conn = SQLiteConnection()
         pool_conn = PoolConnection(connection=mock_conn, connection_id="conn_1")
 
         # Simulate time passing
-        await asyncio.sleep(0.1)
+        await sleep(0.1)
 
         self.assertGreater(pool_conn.age, 0.09)
         self.assertLess(pool_conn.age, 0.15)

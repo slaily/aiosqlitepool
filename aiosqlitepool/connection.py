6e03d920574ca0862685db2853bf0907272617c0
slaily <slavov.iliyan96@gmail.com>
2025-06-26 14:17:29 +0300

    Refactor connection functionality

--- aiosqlitepool/connection.py
+++ aiosqlitepool/connection.py
@@ -1,4 +1,5 @@
 import time
+from typing import Optional
 
 from .protocols import Connection
 
@@ -12,16 +13,19 @@ class PoolConnection:
     def __init__(self, connection: Connection, connection_id: str):
         self.raw_connection = connection
         self.id = connection_id
-        self.created_at = time.time()
+        self.idle_since: Optional[float] = None
+
+    def mark_as_in_use(self) -> None:
+        """Mark the connection as actively in use."""
+        self.idle_since = None
+
+    def mark_as_idle(self) -> None:
+        """Mark the connection as idle."""
+        self.idle_since = time.time()
 
     @property
-    def age(self) -> float:
-        """The age of the connection in seconds."""
-        return time.time() - self.created_at
-
-    @property
-    def is_closed(self) -> bool:
-        """Check if the underlying aiosqlite connection is closed."""
-        # aiosqlite's public API for this is checking the _connection attribute.
-        # When closed, it's not None.
-        return self.raw_connection._connection is not None
+    def idle_time(self) -> float:
+        """The time the connection has been idle in seconds."""
+        if self.idle_since is None:
+            return 0.0
+        return time.time() - self.idle_since
